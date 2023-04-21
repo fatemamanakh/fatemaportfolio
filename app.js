8feac091d3760c4c7d2667ba7b55e66e2516836e
fatemamanakh <114674424+fatemamanakh@users.noreply.github.com>
2023-04-20 22:54:20 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const hamburger = document.querySelector('.headers .navbar .navlist .hamburger');
-const mobile_menu = document.querySelector('.headers .navbar .navlist ul');
-const menu_item = document.querySelectorAll('.headers .navbar .navlist ul li a');
-const header = document.querySelector('.headers.container');
-
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
-});
-
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
+document.addEventListener('DOMContentLoaded', () => {
+	const hamburger = document.querySelector('.headers .navbar .navlist .hamburger');
+	const mobile_menu = document.querySelector('.headers .navbar .navlist ul');
+	const menu_item = document.querySelectorAll('.headers .navbar .navlist ul li a');
+	const header = document.querySelector('.headers.container');
+  
+	hamburger.addEventListener('click', () => {
+	  hamburger.classList.toggle('active');
+	  mobile_menu.classList.toggle('active');
+	});
+  
+	document.addEventListener('scroll', () => {
+	  var scroll_position = window.scrollY;
+	  if (scroll_position > 250) {
 		header.style.backgroundColor = '#29323c';
-	} else {
+	  } else {
 		header.style.backgroundColor = 'transparent';
-	}
-});
-
-menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
+	  }
+	});
+  
+	menu_item.forEach((item) => {
+	  item.addEventListener('click', () => {
 		hamburger.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
+	  });
 	});
-});
+  });
+  
